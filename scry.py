13d5171b8b9733e03fca3cd4727f8f44ada20ce3
Jakub Dudarewicz <jakub.dudarewicz@gmail.com>
2023-06-21 13:51:25 +0200

    Add scry.py

---
+++ scry.py
@@ -0,0 +1,41 @@
+import json
+import argparse
+
+def parse_json(json_file_name:str):
+  with open(args.json_file_name) as json_file:
+    scry_json = json_file.read()
+    scry = json.loads(scry_json)
+  
+  return scry
+
+def json_list_to_names(scry):
+  card_names = []
+
+  with open(args.json_file_name) as json_file:
+    scry_json = json_file.read()
+    scry = json.loads(scry_json)
+
+    for card in scry["data"]:
+      card_names.append(card["name"])
+  
+  return card_names
+
+def print_names(card_names):
+  for card_name in card_names:
+    print(card_name)
+
+if _name_ == "_main_":
+  parser = argparse.ArgumentParser(description='Process JSON lists from scryfall.')
+  parser.add_argument('json_file_name', metavar='JSON', type=str,
+                      help='JSON file to be converted')
+  parser.add_argument('--names', dest="print_names",
+                      help='print names',
+                      action="store_true")
+  args = parser.parse_args()
+  
+  scry = parse_json(args.json_file_name)
+
+  if args.print_names:
+    card_names = json_list_to_names(scry)
+    print_names(card_names)
+
